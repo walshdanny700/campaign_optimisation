5737b35aa086c880a5a7e16cc48b441bad558bbe
danny <walshdanny700@gmail.com>
2022-04-08 22:05:56 +0100

    Fixed Issue with RestDocs 404 error.

--- src/test/java/com/walshdanny700/campaign_optimisation/CampaignOptimisationApplicationTests.java
+++ src/test/java/com/walshdanny700/campaign_optimisation/CampaignOptimisationApplicationTests.java
@@ -1,14 +1,25 @@
 package com.walshdanny700.campaign_optimisation;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
+import static java.text.MessageFormat.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CampaignOptimisationApplicationTests {
+
 
 	@Test
 	void contextLoads() {
@@ -29,4 +40,25 @@ class CampaignOptimisationApplicationTests {
 		CampaignOptimisationApplication.main(new String[] {});
 		assertTrue(true);
 	}
+
+	/**
+	 * This will work when you run ./gradlew build
+	 * It is a sanity check for the spring rest docs
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	void restDocsSanityCheck() throws IOException {
+		String port = System.getProperty("server.port");
+		int serverPort = (port == null) ? Integer.valueOf(8080) : Integer.valueOf(port);
+		String url = format("http://localhost:{0,number,#}/docs/index.html", serverPort);
+
+
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpResponse response = client.execute(new HttpGet(url));
+		int statusCode = response.getStatusLine().getStatusCode();
+		assertThat(statusCode, equalTo(HttpStatus.SC_OK));
+
+	}
+
 }
