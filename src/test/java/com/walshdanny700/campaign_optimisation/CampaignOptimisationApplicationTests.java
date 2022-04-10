5a4c99515123dbdb513973bbac70c6ec40a69ff9
danny <walshdanny700@gmail.com>
2022-04-10 17:13:54 +0100

    Improved CI Pipeline

--- src/test/java/com/walshdanny700/campaign_optimisation/CampaignOptimisationApplicationTests.java
+++ src/test/java/com/walshdanny700/campaign_optimisation/CampaignOptimisationApplicationTests.java
@@ -1,20 +1,11 @@
 package com.walshdanny700.campaign_optimisation;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import java.io.IOException;
-
-import static java.text.MessageFormat.format;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -43,22 +34,5 @@ class CampaignOptimisationApplicationTests {
 		CampaignOptimisationApplication.main(new String[] {});
 		assertTrue(true);
 	}
-
-//	/**
-//	 * This will work when you run ./gradlew build
-//	 * It is a sanity check for the spring rest docs
-//	 *
-//	 * @throws IOException
-//	 */
-//	@Test
-//	void restDocsSanityCheck() throws IOException {
-//		String url = format("http://localhost:{0,number,#}/docs/index.html", port);
-//
-//		HttpClient client = HttpClientBuilder.create().build();
-//		HttpResponse response = client.execute(new HttpGet(url));
-//		int statusCode = response.getStatusLine().getStatusCode();
-//		assertThat(statusCode, equalTo(HttpStatus.SC_OK));
-//
-//	}
 
 }
